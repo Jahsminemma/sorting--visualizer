48226c1059933c1c825e51b85dfda9eee2f957ac
anandman03 <anandmansimar@gmail.com>
2020-08-26 12:25:55 +0530

    Day 3 main link

--- public/partials/header.js
+++ public/partials/header.js
@@ -13,7 +13,7 @@ document.write(`
 
             <ul id="menu">
                 <div> <h4> 
-                    <a href="https://${user.name}.github.io/${user.repo}/index.html"> Searching & Sorting Visualizer </a> 
+                    <a href="https://${user.name}.github.io/${user.repo}/"> Searching & Sorting Visualizer </a> 
                 </h4> </div>
                 <li><a href="/views/linear-search.html">Linear Search</a></li>
                 <li><a href="#">Binary Search</a></li>
