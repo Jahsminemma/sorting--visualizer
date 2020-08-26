982a33d02ff3f4e92e1141583bc1000297d217ca
anandman03 <anandmansimar@gmail.com>
2020-08-26 12:32:17 +0530

    Day 3 linear-search link

--- public/partials/header.js
+++ public/partials/header.js
@@ -15,7 +15,7 @@ document.write(`
                 <div> <h4> 
                     <a href="https://${user.name}.github.io/${user.repo}/"> Searching & Sorting Visualizer </a> 
                 </h4> </div>
-                <li><a href="/views/linear-search.html">Linear Search</a></li>
+                <li><a href="https://${user.name}.github.io/${user.repo}/views/linear-search.html">Linear Search</a></li>
                 <li><a href="#">Binary Search</a></li>
                 <li><a href="#">Bubble Sort</a></li>
                 <li><a href="#"></a></li>
