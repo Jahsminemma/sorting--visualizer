8ecd995e063d897f0de1df62671e01864db006d5
anandman03 <anandmansimar@gmail.com>
2020-08-26 22:44:00 +0530

    Day 5 Random generator

--- public/js/random-array-generator.js
+++ public/js/random-array-generator.js
@@ -2,7 +2,7 @@ function randomize(len)
 {
     let myArray = new Array()
     let lower_Bound = 10
-    let upper_Bound = 100
+    let upper_Bound = 999
 
     for(let i = 0 ; i < len ; i++) {
         let random_Number = Math.floor(Math.random()*(upper_Bound - lower_Bound + 1) + lower_Bound)
