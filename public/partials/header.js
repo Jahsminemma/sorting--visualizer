a05968b9a5bc5f50c0658a7f1fe64a88cfea7473
anandman03 <anandmansimar@gmail.com>
2020-08-26 12:05:04 +0530

    Day 3 corrected links

--- public/partials/header.js
+++ public/partials/header.js
@@ -6,12 +6,13 @@ document.write(`
             <span></span>
             <span></span>
             <ul id="menu">
-                <div> <h4> <a href="./index.html"> Searching & Sorting Visualizer </a> </h4> </div>
+                <div> <h4> <a href="/index.html"> Searching & Sorting Visualizer </a> </h4> </div>
                 <li><a href="./views/linear-search.html">Linear Search</a></li>
                 <li><a href="#">Binary Search</a></li>
                 <li><a href="#">Bubble Sort</a></li>
                 <li><a href="#"></a></li>
             </ul>
         </div>
-    </nav>`
+    </nav>
+    <br>`
 )
