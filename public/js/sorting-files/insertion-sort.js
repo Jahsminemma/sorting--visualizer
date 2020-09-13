01803bb920056e5be291a2c29d2bba6ad62b3be2
anandman03 <anandmansimar@gmail.com>
2020-09-13 15:36:20 +0530

    modified insertion sort

--- public/js/sorting-files/insertion-sort.js
+++ public/js/sorting-files/insertion-sort.js
@@ -1,100 +1,95 @@
 let LOCK = false
-let interval_Time, time_Out
 
 function renderArray()
 {
-    if(LOCK) {
+    if(LOCK) 
+    {
         return
     }
     document.querySelector('.array').innerHTML = ''
 
-    const len = document.querySelector('.generator > input').value
-    const myArray = randomize(len)
+    const arrayLength = document.querySelector('.generator > input').value
+    const myArray = randomize(arrayLength)
     // Base Case
-    if(len > 250)
+    if(arrayLength > 200)
     {
-        alert(`Length of array must be less than 250`)
+        alert(`Length of array must be less than 200`)
         return
     }
 
     const arrayNode = document.querySelector('.array')
-    for(const number of myArray)
+    for(const element of myArray)
     {
         const node = document.createElement('div')
         node.className = 'cell'
-        node.setAttribute('value', String(number))
-        node.style.height = `${3.5*number}px`
+        node.setAttribute('value', String(element))
+        node.style.height = `${3.5*element}px`
         arrayNode.appendChild(node)
     }
 }
 
-function startAlgorithm()
+async function startAlgorithm()
 {
-    let cells = document.querySelectorAll('.cell')
-    // Base Case
-    if(cells.length == 0) 
+    if(LOCK) 
     {
-        alert(`Lenght of Array can't be ${cells.length}`)
+        return
+    }
+    let cells = document.querySelectorAll('.cell')
+    let arrayLength = cells.length
+    // Base Case
+    if(arrayLength == 0)
+    {
+        alert(`Lenght of Array can't be ${arrayLength}`)
         return
     }
 
     const slider = document.querySelector('.slider')
-    if(LOCK) {
-        return
-    }
-    else {
-        slider.disabled = LOCK = true
-        interval_Time = slider.value
-        time_Out = Math.floor((4/5)*interval_Time)
-    }
+    slider.disabled = LOCK = true
+    const intervalTime = slider.value
 
-    let len = cells.length
-    let startIndex = 0, pointer = 0
-
-    const interval = setInterval(function() {
-
-        if(pointer < 0 || Number(cells[pointer].getAttribute('value')) < Number(cells[pointer+1].getAttribute('value')))
+    for(let i = 0 ; i < arrayLength - 1 ; i++)
+    {
+        let j = i
+        while(j >= 0 && Number(cells[j].getAttribute('value')) > Number(cells[j+1].getAttribute('value')))
         {
-            startIndex += 1
-            pointer = startIndex - 1
+            await updateCells(cells, j, j+1, intervalTime)
+            j = j - 1
         }
-        // Base condition
-        if(startIndex == len)
-        {   
-            for(const cell of cells) 
-            {
-                cell.setAttribute('class', `cell done`)
-            }
-            slider.disabled = lock = false
-            return clearInterval(interval)
-        }
-        updateCells(cells, pointer, pointer + 1)
-        pointer = pointer - 1
+    }
 
-    }, interval_Time)
+    for(let i = 0 ; i < arrayLength ; i++)
+    {
+        cells[i].setAttribute('class', `cell done`)
+    }
+    slider.disabled = LOCK = false
 }
 
-function updateCells(cells, firstIndex, secondIndex)
+async function updateCells(cells, firstIndex, secondIndex, intervalTime)
 {
-    cells[firstIndex].setAttribute('class', `cell current`)
-    cells[secondIndex].setAttribute('class', `cell current`)
+    return new Promise(function(resolve) {
 
-    window.setTimeout(function() 
-    {
-        let firstValue = cells[firstIndex].getAttribute('value')
-        let secondValue = cells[secondIndex].getAttribute('value')
-        
-        if(parseInt(firstValue) > parseInt(secondValue))
+        cells[firstIndex].setAttribute('class', `cell current`)
+        cells[secondIndex].setAttribute('class', `cell current`)
+
+        setTimeout(function()
         {
-            cells[secondIndex].setAttribute('value', firstValue)
-            cells[firstIndex].setAttribute('value', secondValue)
+            let firstValue = cells[firstIndex].getAttribute('value')
+            let secondValue = cells[secondIndex].getAttribute('value')
+            
+            if(parseInt(firstValue) > parseInt(secondValue))
+            {
+                cells[secondIndex].setAttribute('value', firstValue)
+                cells[firstIndex].setAttribute('value', secondValue)
 
-            cells[secondIndex].style.height = `${3.5*firstValue}px`
-            cells[firstIndex].style.height = `${3.5*secondValue}px`
-        }
+                cells[secondIndex].style.height = `${3.5*firstValue}px`
+                cells[firstIndex].style.height = `${3.5*secondValue}px`
+            }
 
-        cells[firstIndex].setAttribute('class', `cell`)
-        cells[secondIndex].setAttribute('class', `cell`)
-        
-    }, time_Out)
+            cells[firstIndex].setAttribute('class', `cell`)
+            cells[secondIndex].setAttribute('class', `cell`)
+
+            resolve()
+
+        }, intervalTime)
+    })
 }
